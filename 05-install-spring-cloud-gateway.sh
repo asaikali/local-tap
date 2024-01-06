2e97b8bb2348fa906e066a9d7302340604e88324
asaikali <asaikali@gmail.com>
2024-01-06 02:32:44 -0500

    add spring cloud gateway script

---
+++ 05-install-spring-cloud-gateway.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -euo pipefail
+
+# read the settings.sh file
+readonly GIT_REPO_ROOT=$(git rev-parse --show-toplevel)
+source ${GIT_REPO_ROOT}/config/settings.sh
+
+
+readonly SCGW_VERSION=$(tanzu package available list spring-cloud-gateway.tanzu.vmware.com --namespace tap-install --column version -o json |  jq -r '.[0].version')
+
+#
+# Install Spring Cloud Gateway
+#
+
+tanzu package install spring-cloud-gateway \
+  --package spring-cloud-gateway.tanzu.vmware.com \
+  --version ${SCGW_VERSION} \
+  --namespace tap-install
+  
